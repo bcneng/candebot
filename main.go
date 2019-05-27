4d711aae77726d1946f25ebea35febc7cb11739e
Sergio Moya <smoya89@gmail.com>
2019-05-27 20:32:19 +0200

    Default command gives an ephemeral msg as response in general channel

--- main.go
+++ main.go
@@ -4,6 +4,8 @@ import (
 	"context"
 	"log"
 
+	"github.com/nlopes/slack"
+
 	"github.com/shomali11/slacker"
 
 	"github.com/kelseyhightower/envconfig"
@@ -16,6 +18,8 @@ type specification struct {
 	Debug        bool
 }
 
+var channels = make(map[string]*slack.Channel)
+
 func main() {
 	var s specification
 	err := envconfig.Process("candebot", &s)
@@ -26,7 +30,20 @@ func main() {
 	bot := slacker.NewClient(s.BotUserToken, slacker.WithDebug(s.Debug))
 
 	bot.DefaultCommand(func(request slacker.Request, response slacker.ResponseWriter) {
-		response.Reply("Say what?, try typing `help` to see all the things I can do for you ;)")
+		c, err := channel(bot.Client(), request.Event().Channel)
+		if err != nil {
+			return
+		}
+
+		msg := "Say what?, try typing `help` to see all the things I can do for you ;)"
+		if !c.IsGeneral {
+			response.Reply(msg)
+
+			return
+		}
+
+		_ = sendEphemeral(bot.Client(), request.Event().Channel, request.Event().User, msg)
+
 	})
 
 	bot.Command("coc", &slacker.CommandDefinition{
@@ -47,17 +64,17 @@ func main() {
 		Description: "Info about the staff behind BcnEng",
 		Handler: func(request slacker.Request, response slacker.ResponseWriter) {
 			m := `
-Here is the list of the current staff members: 
+Here is the list of the current staff members:
 
 • Owners
-    • <@gonzaloserrano>
-    • <@smoya>
+   • <@gonzaloserrano>
+   • <@smoya>
 • Admins
-    • <@mavi>
-    • <@sdecandelario>
+   • <@mavi>
+   • <@sdecandelario>
 • Collaborators
-    • <@UAG4H8GMD>
-    • <@U7PQZMZ4L>
+   • <@UAG4H8GMD>
+   • <@U7PQZMZ4L>
 `
 
 			response.Reply(m)
@@ -77,4 +94,28 @@ Here is the list of the current staff members:
 	if err != nil {
 		log.Fatal(err)
 	}
+}
+
+func channel(c *slack.Client, id string) (channel *slack.Channel, err error) {
+	if channel, ok := channels[id]; ok {
+		return channel, nil
+	}
+
+	channel, err = c.GetChannelInfo(id)
+	if err != nil {
+		log.Println("error on retrieving channel info: ", err.Error())
+	}
+
+	channels[id] = channel
+	return
+}
+
+func sendEphemeral(c *slack.Client, channelID, userID, msg string) error {
+	_, err := c.PostEphemeral(channelID, userID, slack.MsgOptionText(msg, true), slack.MsgOptionAsUser(true))
+
+	if err != nil {
+		log.Println("error sending ephemeral msg in channel ", channelID)
+	}
+
+	return err
 }
