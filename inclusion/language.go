4649a61b22d4aab08dd9c0b6db50b7d1ec390000
Sergio Moya <1083296+smoya@users.noreply.github.com>
2021-01-31 05:44:05 +0000

    feat: improve inclusive filter for ladies and gentlemen

--- inclusion/language.go
+++ inclusion/language.go
@@ -48,8 +48,8 @@ var inclusiveFilters = []InclusiveFilter{
 	{Filter: "retraso mental", Reply: "*Retrasado* y *Retraso mental* son términos despectivos eliminados del vocabulario psiquiátrico y, según la OMS, la forma correcta para referiste a ese grupo de enfermedades y transtornos es *Trastorno del desarrollo intelectual* "},
 	
 	// Our own list
-	{Filter: "gentlemen", Reply: "Instead of *gentlemen*, perhaps you mean *folks*?... *[Please consider editing your message so it's more inclusive]*"},
-	{Filter: "ladies", Reply: "Instead of *ladies*, perhaps you mean *folks*?... *[Please consider editing your message so it's more inclusive]*"},
+	{Filter: "gentlem(a|e)n", Reply: "Instead of *gentlem(a|e)n*, perhaps you mean *folks*?... *[Please consider editing your message so it's more inclusive]*"},
+	{Filter: "lad(y|ies)", Reply: "Instead of *lad(y|ies)*, perhaps you mean *folks*?... *[Please consider editing your message so it's more inclusive]*"},
 	{Filter: "los chicos de", Reply: "En vez de *los chicos de*, quizá quisiste decir *el equipo de*, *los integrantes de*?... *[Considera editar tu mensaje para que sea más inclusivo]*"},
 	{Filter: "chicos", Reply: "En vez de *chicos*, quizá quisiste decir *chiques*, *colegas*, *grupo*, *personas*?... *[Considera editar tu mensaje para que sea más inclusivo]*"},
 	{Filter: "lgtb", Reply: "Desde hace un tiempo, el colectivo *LGTB+* recomienda añadir el carácter `+` a la palabra *LGTB*, pues existen orientaciones e identidades que, a pesar de no ser tan predominantes, representan a muchas personas. *[Considera editar tu mensaje para que sea más inclusivo]*"},
