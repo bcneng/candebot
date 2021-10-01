183f8a713ec63acd927646b632d28ff6f66dd15d
Ignasi Marimon-Clos <ignasi35@gmail.com>
2021-10-01 12:29:05 +0200

    Fix code

--- bot/interact.go
+++ bot/interact.go
@@ -82,7 +82,7 @@ func interactAPIHandler(botContext cmd.BotContext) http.HandlerFunc {
 						validationErrors["min_salary"] = "The Salary Min field should contain a lower value than the specified in Salary Max field."
 					}
 
-					if 2.5*float32(minSalary) > float32(maxSalary) {
+					if 2.5*float32(minSalary) < float32(maxSalary) {
 						validationErrors["max_salary"] = "The gap between MinSalary and MaxSalary is rather large. Maybe you should post two different job offers with different responsibilities and required qualifications. Salary is a relevant field, we recommend you try to keep it meaningful to increase the chances of taking the position seriously by potential candidates."
 					}
 				}
