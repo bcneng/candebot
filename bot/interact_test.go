2d3c22e3a61838082fcfe42a7155c1361288fec1
Ignasi Marimon-Clos <ignasi@lightbend.com>
2021-10-03 20:27:04 +0200

    Any error message must be shorter than 150 chars

--- bot/interact_test.go
+++ bot/interact_test.go
@@ -6,7 +6,17 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
+func requireHasError(t *testing.T, errorMessage string) {
+	require.NotEmpty(t, errorMessage)
+
+	if len([]rune(errorMessage)) > 150 {
+		t.Errorf("error message is too long: %s", errorMessage)
+	}
+
+}
+
 func TestJobSubmission(t *testing.T) {
+
 	t.Run("fields for the salary are properly parsed", func(t *testing.T) {
 		max, min, _ := validateSubmission("http://foo.com", "12", "10")
 		require.Equal(t, 12, max)
@@ -20,22 +30,22 @@ func TestJobSubmission(t *testing.T) {
 
 	t.Run("must reject non-URI links", func(t *testing.T) {
 		_, _, validaterrors := validateSubmission("hamburger", "12", "10")
-		require.NotEmpty(t, validaterrors["job_link"])
+		requireHasError(t, validaterrors["job_link"])
 	})
 
 	t.Run("must include a max salary", func(t *testing.T) {
 		_, _, validaterrors := validateSubmission("hamburger", "", "10")
-		require.NotEmpty(t, validaterrors["max_salary"])
+		requireHasError(t, validaterrors["max_salary"])
 	})
 
 	t.Run("max salary must be a positive number", func(t *testing.T) {
 		_, _, validaterrors := validateSubmission("hamburger", "-1", "10")
-		require.NotEmpty(t, validaterrors["max_salary"])
+		requireHasError(t, validaterrors["max_salary"])
 	})
 
 	t.Run("max salary must be a number", func(t *testing.T) {
 		_, _, validaterrors := validateSubmission("hamburger", "-asdf", "10")
-		require.NotEmpty(t, validaterrors["max_salary"])
+		requireHasError(t, validaterrors["max_salary"])
 	})
 
 	t.Run("min salary is optional (and function returns -1 when left blank)", func(t *testing.T) {
@@ -45,30 +55,22 @@ func TestJobSubmission(t *testing.T) {
 
 	t.Run("min salary must be a number", func(t *testing.T) {
 		_, _, validaterrors := validateSubmission("hamburger", "12", "asdf")
-		require.NotEmpty(t, validaterrors["min_salary"])
+		requireHasError(t, validaterrors["min_salary"])
 	})
 
 	t.Run("min salary must be a positive number", func(t *testing.T) {
 		_, _, validaterrors := validateSubmission("hamburger", "12", "-5")
-		require.NotEmpty(t, validaterrors["min_salary"])
+		requireHasError(t, validaterrors["min_salary"])
 	})
 
 	t.Run("max salary must be greater than the min salary", func(t *testing.T) {
 		_, _, validaterrors := validateSubmission("hamburger", "8", "10")
-		require.Equal(t, "The Salary Min field should contain a lower value than the specified in Salary Max field.", validaterrors["min_salary"])
+		requireHasError(t, validaterrors["min_salary"])
 	})
 
 	t.Run("max salary must not be greater than 2.5x the min salary", func(t *testing.T) {
 		_, _, validaterrors := validateSubmission("hamburger", "13", "5")
-		require.Equal(t, "The range MinSalary-MaxSalary is too large. Salary is a relevant field, keep it meaningful to increase offer impact.", validaterrors["max_salary"])
-	})
-
-	t.Run("error messages should be shorter than 150 chars", func(t *testing.T) {
-		_, _, validaterrors := validateSubmission("hamburger", "13", "5")
-		if len([]rune(validaterrors["max_salary"])) > 150 {
-			t.Errorf("error message is too long: %s", validaterrors["max_salary"])
-		}
-
+		requireHasError(t, validaterrors["max_salary"])
 	})
 
 }
