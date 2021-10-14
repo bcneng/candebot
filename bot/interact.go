52bf23c8e1fc37791f010fed3f805ffbaa822b79
Alberto Huélamo <1891056+alhuelamo@users.noreply.github.com>
2021-10-14 11:09:22 +0200

    Sanitizing double encoded links #65 (#75)

--- bot/interact.go
+++ bot/interact.go
@@ -56,7 +56,7 @@ func interactAPIHandler(botContext cmd.BotContext) http.HandlerFunc {
 					message.User.Name,
 					message.Submission["reason"],
 					message.Submission["scale"],
-					message.State,
+					sanitizeReportState(message.State),
 				)
 				_ = slackx.Send(botContext.Client, "", channelStaff, msg, false)
 			case "job_submission":
@@ -285,4 +285,8 @@ func buildPublisherInput() *slack.DialogInputSelect {
 	publisherInput.Optional = false
 
 	return publisherInput
+}
+
+func sanitizeReportState(state string) string {
+	return strings.ReplaceAll(state, "\\/", "/")
 }
