0225626139ca8525e2e41722d4618e1fad12cc10
Sergio Moya <1083296+smoya@users.noreply.github.com>
2024-04-09 12:57:36 +0200

    feat: add utm_source to job offer links (#171)
    * feat: add utm_source to job offer links
    
    * ci: update go GH action

--- bot/interact_test.go
+++ bot/interact_test.go
@@ -18,60 +18,63 @@ func requireHasError(t *testing.T, errorMessage string) {
 func TestJobSubmission(t *testing.T) {
 
 	t.Run("fields for the salary are properly parsed", func(t *testing.T) {
-		max, min, _ := validateSubmission("http://foo.com", "12", "10")
+		link, max, min, _ := validateSubmission("http://foo.com", "12", "10")
+		require.NotNil(t, link)
 		require.Equal(t, 12, max)
 		require.Equal(t, 10, min)
 	})
 
 	t.Run("the job link must be a valid URI", func(t *testing.T) {
-		_, _, validaterrors := validateSubmission("http://foo.com", "12", "10")
+		link, _, _, validaterrors := validateSubmission("http://foo.com", "12", "10")
 		require.Empty(t, validaterrors["job_link"])
+		require.NotNil(t, link)
 	})
 
 	t.Run("min salary is optional (and function returns -1 when left blank)", func(t *testing.T) {
-		_, minSalary, _ := validateSubmission("hamburger", "12", "")
+		_, _, minSalary, _ := validateSubmission("http://foo.com", "12", "")
 		require.Equal(t, -1, minSalary)
 	})
 
 	// FIXME: The following tests may be rewritten as a table-driven test
 	// https://github.com/bcneng/candebot/pull/72#pullrequestreview-773870224
 	t.Run("must reject non-URI links", func(t *testing.T) {
-		_, _, validaterrors := validateSubmission("hamburger", "12", "10")
+		link, _, _, validaterrors := validateSubmission("hamburger", "12", "10")
 		requireHasError(t, validaterrors["job_link"])
+		require.Nil(t, link)
 	})
 
 	t.Run("must include a max salary", func(t *testing.T) {
-		_, _, validaterrors := validateSubmission("hamburger", "", "10")
+		_, _, _, validaterrors := validateSubmission("http://foo.com", "", "10")
 		requireHasError(t, validaterrors["max_salary"])
 	})
 
 	t.Run("max salary must be a positive number", func(t *testing.T) {
-		_, _, validaterrors := validateSubmission("hamburger", "-1", "10")
+		_, _, _, validaterrors := validateSubmission("http://foo.com", "-1", "10")
 		requireHasError(t, validaterrors["max_salary"])
 	})
 
 	t.Run("max salary must be a number", func(t *testing.T) {
-		_, _, validaterrors := validateSubmission("hamburger", "-asdf", "10")
+		_, _, _, validaterrors := validateSubmission("http://foo.com", "-asdf", "10")
 		requireHasError(t, validaterrors["max_salary"])
 	})
 
 	t.Run("min salary must be a number", func(t *testing.T) {
-		_, _, validaterrors := validateSubmission("hamburger", "12", "asdf")
+		_, _, _, validaterrors := validateSubmission("http://foo.com", "12", "asdf")
 		requireHasError(t, validaterrors["min_salary"])
 	})
 
 	t.Run("min salary must be a positive number", func(t *testing.T) {
-		_, _, validaterrors := validateSubmission("hamburger", "12", "-5")
+		_, _, _, validaterrors := validateSubmission("http://foo.com", "12", "-5")
 		requireHasError(t, validaterrors["min_salary"])
 	})
 
 	t.Run("max salary must be greater than the min salary", func(t *testing.T) {
-		_, _, validaterrors := validateSubmission("hamburger", "8", "10")
+		_, _, _, validaterrors := validateSubmission("http://foo.com", "8", "10")
 		requireHasError(t, validaterrors["min_salary"])
 	})
 
 	t.Run("max salary must not be greater than 2.5x the min salary", func(t *testing.T) {
-		_, _, validaterrors := validateSubmission("hamburger", "13", "5")
+		_, _, _, validaterrors := validateSubmission("http://foo.com", "13", "5")
 		requireHasError(t, validaterrors["max_salary"])
 	})
 
